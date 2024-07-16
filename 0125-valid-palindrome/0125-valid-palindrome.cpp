73feb2578d317d8c9ecd012a04f501efb08bb96d
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-07-17 02:23:49 +0600

    Time: 8 ms (34.24%), Space: 8.9 MB (38.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int size = s.size();
+        int k = size-1;
+        while(l<k){
+            while(!isalnum(s[l]) && l<k){
+                l++;
+            }
+            while(!isalnum(s[k]) && l<k){
+                k--;
+            }
+            if(tolower(s[l]) != tolower(s[k])){
+                return false;
+            }
+            l++;
+            k--;
+        }
+        
+        return true;
+       
+    }
+};
