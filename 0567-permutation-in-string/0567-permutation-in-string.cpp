31a80dee19900e224d3d08bd76e45ea9b24f1ae4
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-08-15 23:54:47 +0600

    Time: 3 ms (94.13%), Space: 8.8 MB (45.36%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int size1 = s1.size();
+        int size2 = s2.size();
+        
+        vector<int>v1(26, 0);                
+        vector<int>v2(26, 0);
+        int left = 0, right = 0;
+        if(size1>size2)return false;
+        
+        while(right<size1){
+            v1[s1[right]-'a']++;
+            v2[s2[right]-'a']++;
+            right++;
+        }
+        right--;
+        
+        while(right<size2){
+            if(v1 == v2)return true;
+            right++;
+            
+            if(right!=size2){
+                v2[s2[right] - 'a']++;       
+            }
+            v2[s2[left] - 'a']--;
+            left++;
+            
+        }
+        return false;
+        
+    }
+};
