6920345d403bffd1f0e881c2f41420a22147945c
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-08-27 20:14:31 +0600

    Time: 100 ms (84.90%), Space: 84.4 MB (35.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -19,19 +19,19 @@ public:
         stack<int>st1;
         for(int i = n-1 ;i>=0 ; i--){
             if(st1.empty()){
-                right[i] = n-1;
+                right[i] = n;
                 st1.push(i);
             }else{
                 while(!st1.empty() && heights[st1.top()] >= heights[i]){
                     st1.pop();
                 }
-                right[i] = st1.empty()?n-1:st1.top()-1;
+                right[i] = st1.empty()?n:st1.top();
                 st1.push(i);
             }
         }
         int mx = 0;
         for(int i = 0; i < n ; i++){
-            int d = (right[i] - left[i] + 1) * heights[i];
+            int d = (right[i] - left[i]) * heights[i];
             mx = max(mx, d);
         }
         return mx;
