0a062be852a4f1f91522789dba588f6b6a8bdf4d
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-09-25 16:23:47 +0600

    Time: 3 ms (91.44%), Space: 14.6 MB (84.72%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,81 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+        if(head == NULL){
+            return NULL;
+        }
+        
+        Node* curr = head;
+        // Creating a new duplicate Node right after each
+        while(curr){
+            Node* nxt = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = nxt;
+            curr = nxt;
+        }
+        
+        curr = head;
+        // Assigning random pointer
+        while(curr){
+            if(curr->random == NULL){
+                curr->next->random = NULL;
+            }else{
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        
+        curr = head;
+        Node* newhead = head->next;
+        Node* newcurr = newhead;
+        
+        while(curr && newcurr){
+            
+            curr->next = curr->next == NULL ? NULL: curr->next->next;
+            newcurr->next = newcurr->next == NULL ? NULL: newcurr->next->next;
+            curr = curr->next;
+            newcurr = newcurr->next;
+        }
+        
+        
+        
+        return newhead;
+        
+        
+        
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
