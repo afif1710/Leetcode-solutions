127cf5e8a516b41b2ee850a7aef71e454b29f922
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-09-28 09:16:46 +0600

    Time: 8 ms (21.23%), Space: 14.8 MB (80.56%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if(!head){
+            return NULL;
+        }
+        if(!head->next){
+            return head;
+        }
+        ListNode* small = new ListNode(0);
+        ListNode* large = new ListNode(0);
+        
+        ListNode* smlptr = small;
+        ListNode* lrgptr = large;
+        
+        while(head){
+            if(head->val < x){
+                smlptr->next = head;
+                smlptr = smlptr->next;
+            }else{
+                lrgptr->next = head;
+                lrgptr = lrgptr->next;    
+            }
+            head = head->next;
+        }
+        smlptr->next = large->next;
+        lrgptr->next = NULL;
+        
+        return small->next;       
+    }
+};
