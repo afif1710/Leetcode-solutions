63ede8a194505c0a5b0419c4d07c9131e3c64121
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-09-29 10:52:00 +0600

    Time: 31 ms (16.99%), Space: 80.1 MB (6.64%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,35 +8,75 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
- 
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* temp = dummy;
-        int sum ;
-        int carry = 0;
+    ListNode* revlist(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* nxt = NULL;
+        while(curr){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+    ListNode* dln(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr->next != NULL){
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = NULL;
+        delete curr;
+        return head;
         
-        while(l1 != NULL || l2 != NULL || carry){
-            sum = 0;
-            if(l1){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if(l2){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum+=carry;
-            carry = sum/10;
-            
-            ListNode* newNode = new ListNode(sum%10);
-            temp->next = newNode;
-            temp = newNode;
-            
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = revlist(l1);
+        l2 = revlist(l2);
+
+        stack<int>s1;
+        stack<int>s2;
+        int sum = 0;
+        int carry = 0;
+        ListNode* ans = new ListNode();
+        while(l1){
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while(l2){
+            s2.push(l2->val);
+            l2 = l2->next;
+
         }
         
-        return dummy->next;
+        while(!s1.empty() || !s2.empty()){
+            if(!s1.empty()){
+                sum += s1.top();
+                s1.pop();
+                
+            }
+            if(!s2.empty()){
+                sum += s2.top();
+                s2.pop();
+            }
+            ans->val = sum % 10;         
+            carry = sum/10;
+            ListNode* newNode = new ListNode(carry);
+            newNode->next = ans;
+            ans = newNode;
+            sum = carry;
+        }
         
+        ListNode* ta = revlist(ans);
+        if(carry == 0){
+            dln(ta);
+        }
+        //return carry == 0 ? ta : ta->next;
+        return ta;
     }
 };
