460a2542ee791a894cc4ccf4a03d5104ad8f45ca
Afif <119808088+White-Wolf99@users.noreply.github.com>
2024-12-27 09:55:14 +0600

    Time: 0 ms (100.00%), Space: 11.4 MB (18.55%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    int n;
+    unordered_set<int> st;
+    void solve(vector<int>& temp, vector<int>& nums){
+        if(temp.size() == n){
+            res.push_back(temp);
+            return;
+        }
+        
+        for(int i = 0; i<n; i++){
+            if(st.find(nums[i]) == st.end()){
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+                solve(temp, nums);
+                temp.pop_back();
+                st.erase(nums[i]);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        vector<int> temp;
+        solve(temp, nums);
+            
+        return res;
+        
+    }
+};
