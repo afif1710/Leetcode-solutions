954a0b4d6b730239332119693fa1f2979eaf5e3e
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-02-06 18:52:27 +0600

    Time: 0 ms, Memory: 34.3 MB - LeetHub

---
+++ 9541-323-450-delete-node-in-a-bst/9541-323-450-delete-node-in-a-bst.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL){
+            return NULL;
+        }
+        if(key > root->val){
+            root->right = deleteNode(root->right, key);
+        }else if(key < root->val){
+            root->left = deleteNode(root->left, key);
+        }else{
+            if(!root->left){
+                return root->right;
+            }
+            if(!root->right){
+                return root->left;
+            }
+            TreeNode* tmp = root->right;
+
+            while(tmp->left){
+                tmp = tmp->left;
+            }
+            root->val = tmp->val;
+            root->right = deleteNode(root->right, tmp->val);
+        }
+
+        return root;
+        
+        
+    }
+};
