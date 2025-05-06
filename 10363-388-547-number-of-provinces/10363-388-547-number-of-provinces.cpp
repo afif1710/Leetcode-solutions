2bc045412b0c629af9ed6b456ca0def2d1b23b10
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-05-06 22:49:04 +0600

    Time: 2 ms, Memory: 19.2 MB - LeetHub

---
+++ 10363-388-547-number-of-provinces/10363-388-547-number-of-provinces.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int n;
+    void dfs(vector<vector<int>>& isConnected, int u, vector<bool>& visited){
+        visited[u] = true;
+
+        for(int v = 0; v<n; v++){
+            if(!visited[v] && isConnected[u][v]){
+                dfs(isConnected, v, visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n = isConnected.size();
+        vector<bool> visited(n, false);
+        int cnt = 0;
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                dfs(isConnected, i, visited);
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
