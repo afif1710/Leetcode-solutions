330bb6b3eaacabd3ab747cef92ce604ab30186b0
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-08-30 18:22:44 +0600

    Update 4676-135-646-maximum-length-of-pair-chain.cpp

--- 4676-135-646-maximum-length-of-pair-chain/4676-135-646-maximum-length-of-pair-chain.cpp
+++ 4676-135-646-maximum-length-of-pair-chain/4676-135-646-maximum-length-of-pair-chain.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int n, m;
-    int t[1001][1001];
-    int solve(vector<vector<int>>& pairs, int i, int p){
-        if(i>=n){
+    int n;
+    int dp[1001][1001];
+    int solve(int prev, int curr, vector<vector<int>> &pairs){
+        if(curr >= n){
             return 0;
         }
-        if(p > -1 && t[i][p] != -1){
-            return t[i][p];
+        if(prev >= 0 && dp[prev][curr] != -1){
+            return dp[prev][curr];
         }
-        int take = 0, skip = 0;
-        if(p == -1 || pairs[i][0] > pairs[p][m-1]){
-            take = 1+solve(pairs, i+1, i);
+        int take = 0 , skip = 0;
+        if(prev == -1 || pairs[prev][1] < pairs[curr][0]){
+            take = 1 + solve(curr, curr + 1, pairs);        // when we take, prev -> curr and curr -> curr+1
         }
-        skip = solve(pairs, i+1, p);
-        if(p > -1){
-            t[i][p] = max(take, skip);
+        skip = solve(prev, curr + 1, pairs);                // when we skip, prev remains same & curr -> curr+1
+        if(prev >= 0){
+            dp[prev][curr] = max(take, skip);       // when prev != -1
         }
-        return max(take, skip);
-
+        return max(take, skip);                     // including when prev = -1
     }
-
     int findLongestChain(vector<vector<int>>& pairs) {
-        memset(t, -1, sizeof(t));
+        memset(dp, -1, sizeof(dp));
         n = pairs.size();
-        m = pairs[0].size();
         sort(pairs.begin(), pairs.end());
-        return solve(pairs, 0, -1);
+        return solve(-1, 0, pairs);
     }
 };
