5ccff3d5c4a22042b040ad5edd57f1758cebc31e
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-09-10 05:53:43 +0600

    Time: 46 ms (16.98%) | Memory: 11.5 MB (86.76%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int m, n;
+    
     int dp[501][501];
-    int solve(string &s1, string &s2, int i, int j){
-        if(j >= n){
-            return m - i; 
+    int solve(string &s1, string &s2, int m, int n){
+        if(n < 0){
+            return m + 1; 
         }
-        if(i >= m){
-            return n - j;
+        if(m < 0){
+            return n + 1;
         }
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[m][n] != -1){
+            return dp[m][n];
         }
-        if(s1[i] == s2[j]){
-            return dp[i][j] = solve(s1, s2, i+1, j+1);
+        if(s1[m] == s2[n]){
+            return dp[m][n] = solve(s1, s2, m - 1, n - 1);
         }
-        int insertion = 1 + solve(s1, s2, i, j+1);
-        int deletion = 1 + solve(s1, s2, i+1, j);
-        int replace = 1 + solve(s1, s2, i+1, j+1);
+        int insertion = 1 + solve(s1, s2, m, n - 1);
+        int deletion = 1 + solve(s1, s2, m - 1, n);
+        int replace = 1 + solve(s1, s2, m - 1, n - 1);
         
-        return dp[i][j] = min({insertion, deletion, replace});
+        return dp[m][n] = min({insertion, deletion, replace});
         
     }
     int minDistance(string s1, string s2) {
-        m = s1.length();
-        n = s2.length();
+        int m = s1.length();
+        int n = s2.length();
         memset(dp, -1, sizeof(dp));
-        return solve(s1, s2, 0, 0);
+        return solve(s1, s2, m-1, n-1);
     }
 };
