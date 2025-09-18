049fa85770e63be5009d55905ae0f781d5fa7e22
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-09-18 20:24:12 +0600

    Time: 5 ms (7.81%) | Memory: 20.5 MB (5.20%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int n;
+    bool DFS(unordered_map<int, vector<int>> &adj, vector<int> &color, int currNode, int currColor){
+        color[currNode] = currColor;
+        for(int &v: adj[currNode]){
+            if(color[v] == color[currNode]){
+                return false;
+            }
+            if(color[v] == -1 && !DFS(adj, color, v, !currColor)){
+                return false;
+            } 
+            
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        n = graph.size();
+        unordered_map<int, vector<int>> adj;
+
+        for(int u = 0; u<n ; u++){
+            for(int v = 0; v<graph[u].size(); v++){
+                adj[u].push_back(graph[u][v]);
+            }
+        }
+        vector<int> color(n, -1);
+        for(int i = 0 ; i<n; i++){
+            if(color[i] == -1 && !DFS(adj, color, i, 1)){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
