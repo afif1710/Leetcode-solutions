e523f09e309fb99dbbdd693296a4bcbe47ebc225
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-09-20 01:24:48 +0600

    Time: 11 ms (5.02%) | Memory: 20.9 MB (5.18%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int n;
-    bool DFS(unordered_map<int, vector<int>> &adj, vector<int> &color, int currNode, int currColor){
+    bool BFS(unordered_map<int, vector<int>> &adj, vector<int> &color, int currNode, int currColor){
+        queue<int> que;
         color[currNode] = currColor;
-        for(int &v: adj[currNode]){
-            if(color[v] == color[currNode]){
-                return false;
+        que.push(currNode);
+        
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            for(int &v: adj[u]){
+                if(color[v] == color[u]){
+                    return false;
+                }
+                if(color[v] == -1){
+                    int newColor = 1 - color[u];
+                    color[v] = newColor;
+                    que.push(v);
+                }
             }
-            if(color[v] == -1 && !DFS(adj, color, v, !currColor)){
-                return false;
-            } 
-            
         }
+
         return true;
+
+        
     }
     bool isBipartite(vector<vector<int>>& graph) {
         n = graph.size();
@@ -25,7 +36,7 @@ public:
         }
         vector<int> color(n, -1);
         for(int i = 0 ; i<n; i++){
-            if(color[i] == -1 && !DFS(adj, color, i, 1)){
+            if(color[i] == -1 && !BFS(adj, color, i, 1)){
                 return false;
             }
         }
