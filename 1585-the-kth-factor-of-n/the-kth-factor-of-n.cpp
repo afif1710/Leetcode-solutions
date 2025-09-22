9c8c177436b124cfc8b9ee39f8d3a0c819be2a97
Afif <119808088+White-Wolf99@users.noreply.github.com>
2025-09-22 09:05:47 +0600

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.99%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int ans;
+        for(int i = 1; i*i < n; i++){
+            
+            if(n%i == 0){
+                k--;
+            }
+            if(k == 0){
+                return i;
+            }
+            
+        }
+        
+       
+        
+        if(k > 0){
+            for(int i = sqrt(n); i > 0; i--){
+                
+                if(n%i == 0){
+                    k--;
+                }
+                if(k == 0){
+                    return n/i;
+                }
+                
+            }
+        }
+        if(k < 0){
+            ans = -1;
+        }
+        return ans;
+    }
+};
