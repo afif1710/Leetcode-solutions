4ab9ad7d3aa308b008bbeb6a243ea98d2c8ef4fa
Md. Afif <233668089+afif1710@users.noreply.github.com>
2025-10-21 09:48:20 +0600

    Time: 0 ms (100.00%) | Memory: 12.7 MB (52.29%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -4,17 +4,17 @@ public:
         int n = deck.size();
         sort(deck.begin(), deck.end());
         queue<int> qu;
-        for(int i = 0; i<n ; i++){
+        for(int i = 0; i<n ; i++){      //Fill queue with indices
             qu.push(i);
         }
         vector<int> res(n);
         for(int i = 0 ; i<n ; i++){
-            int idx = qu.front();
+            int idx = qu.front();   //Get the top index 
             qu.pop();
-            res[idx] = deck[i];
-            if(!qu.empty()){
-                qu.push(qu.front());
-                qu.pop();
+            res[idx] = deck[i];     //then input ith value
+            if(!qu.empty()){        //if queue is not empty
+                qu.push(qu.front());    //then take the top element push in the queue
+                qu.pop();           //then pop it
             }
         }
         return res;
