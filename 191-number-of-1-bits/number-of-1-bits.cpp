102fad7b9236adfd5fde3c70cd60a5d6798d377b
Md. Afif <233668089+afif1710@users.noreply.github.com>
2025-10-25 17:13:23 +0600

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.88%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
+    int countOneBits(int n){
+        int countOfOne = 0;
+        for(int i = 1; i<32; i++){
+            if((n & 1) == 1){
+                countOfOne++;
+            }
+            n >>= 1;
+        }
+        return countOfOne;
+
+    }
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        return countOneBits(n);
         
     }
 };
