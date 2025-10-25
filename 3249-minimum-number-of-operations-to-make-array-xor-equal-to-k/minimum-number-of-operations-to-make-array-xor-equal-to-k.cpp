0a1b619d15dceeb36b9f5d016598dd0a0007f2f7
Md. Afif <233668089+afif1710@users.noreply.github.com>
2025-10-25 17:23:59 +0600

    Time: 106 ms (56.01%) | Memory: 93 MB (38.22%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countOneBits(int n){
+        int count = 0;
+        for(int i = 1; i< 32; i++){
+            if((n & 1) == 1){
+                count++;
+            }
+            n >>= 1;
+        }
+        return count;       // Return number of 1 bits
+    }
+    int minOperations(vector<int>& nums, int k) {
+        int totalXOR = 0;
+
+        for(auto &num: nums){
+            totalXOR = totalXOR ^ num;      // XOR of all numbers
+        }
+        int difference = totalXOR ^ k;      // Stores the XOR of totalXOR and the desired value 
+        // Number of 1 bits is the number of required flips 
+        int numberOfOneBits = countOneBits(difference); 
+
+        return numberOfOneBits;
+
+    }
+};
