a7fad9148a37394d71071837acf2834092040852
Md. Afif <233668089+afif1710@users.noreply.github.com>
2025-10-25 17:24:57 +0600

    Time: 110 ms (40.41%) | Memory: 93.1 MB (38.22%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int countOneBits(int n){
-        int count = 0;
-        for(int i = 1; i< 32; i++){
-            if((n & 1) == 1){
-                count++;
-            }
-            n >>= 1;
-        }
-        return count;       // Return number of 1 bits
-    }
+    // int countOneBits(int n){
+    //     int count = 0;
+    //     for(int i = 1; i< 32; i++){
+    //         if((n & 1) == 1){
+    //             count++;
+    //         }
+    //         n >>= 1;
+    //     }
+    //     return count;       // Return number of 1 bits
+    // }
     int minOperations(vector<int>& nums, int k) {
         int totalXOR = 0;
 
@@ -18,7 +18,9 @@ public:
         }
         int difference = totalXOR ^ k;      // Stores the XOR of totalXOR and the desired value 
         // Number of 1 bits is the number of required flips 
-        int numberOfOneBits = countOneBits(difference); 
+        // int numberOfOneBits = countOneBits(difference); 
+
+        int numberOfOneBits = __builtin_popcount(difference);
 
         return numberOfOneBits;
 
