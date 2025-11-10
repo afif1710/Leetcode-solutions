9a60756cfd0e06a4ea7289f541dfdab4043e0f16
Md. Afif <233668089+afif1710@users.noreply.github.com>
2025-11-10 22:11:22 +0600

    Time: 32 ms (62.69%) | Memory: 19.3 MB (52.71%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int findTheLongestSubstring(string s) {
         int n = s.size();
-        int mask = 0;
-        int final_length = 0;
-        unordered_map<int, int> mp;
-        mp[mask] = -1;
+        int mask = 0;  // bitmask to track parity (even/odd) of vowels
+        unordered_map<int, int> mp; 
+        mp[mask] = -1; // base case: empty prefix has mask 0 at index -1
         int res = 0;
-        for(int i = 0; i<n; i++){
-            if(s[i] == 'a'){
-                mask = mask ^ (1 << 0);
-            }else if(s[i] == 'e'){
-                mask = mask ^ (1 << 1);
-            }else if(s[i] == 'i'){
-                mask = mask ^ (1 << 2);
-            }else if(s[i] == 'o'){
-                mask = mask ^ (1 << 3);
-            }else if(s[i] == 'u'){
-                mask = mask ^ (1 << 4);
-            }
-            if(mp.find(mask) != mp.end()){
+
+        for (int i = 0; i < n; i++) {
+            // flip corresponding vowel bit when encountered
+            if (s[i] == 'a') mask ^= (1 << 0);
+            else if (s[i] == 'e') mask ^= (1 << 1);
+            else if (s[i] == 'i') mask ^= (1 << 2);
+            else if (s[i] == 'o') mask ^= (1 << 3);
+            else if (s[i] == 'u') mask ^= (1 << 4);
+
+            // if same mask seen before → substring between has even count of vowels
+            if (mp.find(mask) != mp.end()) {
                 res = max(res, i - mp[mask]);
-            }else{
+            } 
+            else {
+                // store first occurrence of this mask
                 mp[mask] = i;
             }
         }
-        return res;
+
+        return res; // longest substring with even vowel counts
     }
 };
