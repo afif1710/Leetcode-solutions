224bd0c7f69f647d626c078bef89c550da48539c
Md. Afif <233668089+afif1710@users.noreply.github.com>
2026-01-06 08:18:06 +0600

    Time: 0 ms (100.00%) | Memory: 22.1 MB (38.45%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
-        stack<int> st;
+        stack<int> st;          // will store surviving asteroids (left -> right order)
         vector<int> ans;
-       
-        for(int i = 0; i<n ; i++){
-            int a = asteroids[i];
 
-            // positive asteroids can't collide with previous ones, so push directly
+        for (int a : asteroids) {
+
+            // Case 1: moving right => cannot collide with previous asteroids
             if (a > 0) {
                 st.push(a);
                 continue;
             }
-            while(!st.empty() && asteroids[i] < 0 && st.top() < abs(asteroids[i]) && st.top() > 0){
 
+            // Case 2: moving left => may collide with previous right-movers
+            // Keep destroying smaller right-movers on top.
+            while (!st.empty() && st.top() > 0 && st.top() < -a) {
                 st.pop();
-                
             }
-            if(!st.empty() && st.top() == abs(asteroids[i])){
+
+            // Now decide the fate of 'a' after all possible pops.
+            if (st.empty() || st.top() < 0) {
+                // No right-mover to collide with => 'a' survives
+                st.push(a);
+            } else if (st.top() == -a) {
+                // Equal size => both explode
                 st.pop();
-            }else if(st.empty() || st.top() < 0){
-                st.push(asteroids[i]);
             }
-            
+            // Else: st.top() > -a (bigger right-mover) => 'a' is destroyed (do nothing)
         }
-        while(!st.empty()){
+
+        // Stack -> vector (this reverses order)
+        while (!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
-        reverse(ans.begin(), ans.end()); 
+        reverse(ans.begin(), ans.end());  // restore left-to-right order
+
         return ans;
     }
 };
