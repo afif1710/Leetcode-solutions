9ad5b5c69eb5b9247169e4d8f945a8d4a4c81ab8
Md. Afif <233668089+afif1710@users.noreply.github.com>
2026-02-16 08:20:03 +0600

    Time: 4 ms (11.75%) | Memory: 25.1 MB (5.15%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> v;
+        for(int i = 0; i<n ; i++){
+            if(nums[i] != 0){
+                v.push_back(nums[i]);
+            }
+        }
+        int rem = n - v.size();
+        while(rem--){
+            v.push_back(0);
+        }
+        nums = v;
+        
+    }
+};
