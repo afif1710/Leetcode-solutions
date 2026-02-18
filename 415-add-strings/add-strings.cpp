d744a3877512cce1e24ab9135c0117eb81b0e571
Md. Afif <233668089+afif1710@users.noreply.github.com>
2026-02-18 11:34:34 +0600

    Time: 0 ms (100.00%) | Memory: 9.3 MB (40.75%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+
+        int carry = 0;
+        int sum;
+        string str = "";
+        while(i >= 0 || j >= 0 || carry){
+            sum = carry;
+            if(i>=0){
+                sum += num1[i] - '0';
+            }
+            if(j>=0){
+                sum += num2[j] - '0';
+            }
+         
+            carry = sum/10;
+            str.push_back((sum%10) + '0');
+            i--;
+            j--;
+            
+        }
+        reverse(str.begin(), str.end());
+
+        return str;
+    }
+};
